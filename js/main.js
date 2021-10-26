9194f21a10ebcdfaa21939572ecbf8684405710a
Im-joker <2865467965@qq.com>
2021-10-26 11:19:33 +0000

    修复失败，看不懂js靠

--- js/main.js
+++ js/main.js
@@ -13,11 +13,7 @@ $(document).ready(function() {
   $('a.blog-button').click(function() {
      
     // If already in blog, return early without animate overlay panel again.
-    if (location.hash && location.hash == "#blog") 
-    $.pjax({
-      // url: '访问后台的url', 
-       container: '#pageContent'
-    }); 
+    if (location.hash && location.hash == "#blog") return; 
 
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
     $('.main-post-list').removeClass('hidden');
