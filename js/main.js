1a191b969939b7c28b9dab7358d7e8ff9ded011c
Im-joker <2865467965@qq.com>
2021-11-03 17:04:31 +0800

    艹

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-  $(document).on('pjax:complete', function () {
+﻿  $(document).on('pjax:complete', function () {
   // 重新加载不蒜子
   $.getScript('//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js');
   });
@@ -6,7 +6,7 @@
 $(document).ready(function() {
  
   $('a.blog-button').click(function() {
-    $.pjax.reload('#pageContent',options);
+    //$.pjax.reload('#pageContent',options);
     // If already in blog, return early without animate overlay panel again.
     if (location.hash && location.hash == "#blog") return; 
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
