c5a798bb8b0726b6ca8a0c0445cb6f677c035cfa
Im-joker <2865467965@qq.com>
2021-11-07 00:21:47 +0800

    去除冗余

--- js/main.js
+++ js/main.js
@@ -5,8 +5,6 @@
   
 $(document).ready(function() {
   $('a.blog-button').click(function() {
-    //$.pjax.reload('#pageContent',options);
-    // If already in blog, return early without animate overlay panel again.
     if (location.hash && location.hash == "#blog") return; 
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
     $('.main-post-list').removeClass('hidden');
