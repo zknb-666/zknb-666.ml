e0ee36580bc2bfffa19f7de95d9c5bb1dcca0e20
Im-joker <2865467965@qq.com>
2021-12-17 21:39:29 +0800

    修复网页计数无法显示的问题

---
+++ js/hide.js
@@ -0,0 +1,6 @@
+if ($('.panel-cover').hasClass('panel-cover--collapsed')) {
+    $(".cover-clear").addClass("panel-cover--overlay");
+    $('.panel-cover').removeClass('cnm');
+    $('.panel-cover').addClass('cnmb');
+    $(".main-post-list").removeClass("hidden");
+    $('.footer').removeClass('hidden');}
--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-﻿   $(document).on("pjax:complete",
+﻿    $(document).on("pjax:complete",
     function() {
-        $.getScript("/js/main.js");
         $.getScript("https://busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js");
+        $.getScript("/js/hide.js");
     });
-$(document).ready(function() { 
     $("a.blog-button").click(function() {
         currentWidth = $(".panel-cover").width();
         if (currentWidth < 2000) {
@@ -14,8 +13,7 @@ $(document).ready(function() {
                 "max-width": "320px",
                 "width": "22%"
             },
-            400, swing = "swing",
-            function() {})
+            400, swing = "swing");
         }
     });
     $("a.menu").click(function() {
@@ -28,18 +26,9 @@ $(document).ready(function() {
                 "max-width": "320px",
                 "width": "22%"
             },
-            400, swing = "swing",
-            function() {})
+            400, swing = "swing");
         }
     });
     if (window.location.hash && window.location.hash == "#blog") {
         $(".panel-cover").addClass("panel-cover--collapsed");
     }
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) {
-        $(".cover-clear").addClass("panel-cover--overlay");
-        $('.panel-cover').removeClass('cnm');
-        $('.panel-cover').addClass('cnmb');
-        $(".main-post-list").removeClass("hidden");
-        $('.footer').removeClass('hidden');
-    }
-});
