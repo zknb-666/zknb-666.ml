c6165100d584c169232da040fa7ba446ab2efd28
zknb-666 <2865467965@qq.com>
2022-08-08 21:57:01 +0800

    添加黑夜模式

--- js/main.js
+++ js/main.js
@@ -57,3 +57,26 @@
         $(".shang_box").fadeToggle();
     }
     /*打赏结束*/
+    /*黑夜模式*/
+    function addDarkmodeWidget() {
+        const options = {
+            bottom: '64px', // default: '32px'
+            right: 'unset', // default: '32px'
+            left: '32px', // default: 'unset'
+            time: '0.5s', // default: '0.3s'
+            mixColor: '#fff', // default: '#fff'
+            backgroundColor: '#fff',  // default: '#fff'
+            buttonColorDark: '#100f2c',  // default: '#100f2c'
+            buttonColorLight: '#fff', // default: '#fff'
+            saveInCookies: false, // default: true,
+            label: '🌓', // default: ''
+            autoMatchOsTheme: true // default: true
+          }
+          
+          const darkmode = new Darkmode(options);
+          darkmode.showWidget();
+          
+        new Darkmode().showWidget();
+      }
+      window.addEventListener('load', addDarkmodeWidget);
+    /*黑夜模式结束*/
