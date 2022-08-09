78a0b5a2714266809e1acfa6c8eb88e2a1191b1a
zknb-666 <2865467965@qq.com>
2022-08-09 11:35:45 +0800

    暗黑模式按钮被图片覆盖修复及优化

--- js/main.js
+++ js/main.js
@@ -60,14 +60,6 @@
     /*黑夜模式*/
     function addDarkmodeWidget() {
         const options = {
-            bottom: '64px', // default: '32px'
-            right: 'unset', // default: '32px'
-            left: '32px', // default: 'unset'
-            time: '0.5s', // default: '0.3s'
-            mixColor: '#fff', // default: '#fff'
-            backgroundColor: '#fff',  // default: '#fff'
-            buttonColorDark: '#100f2c',  // default: '#100f2c'
-            buttonColorLight: '#fff', // default: '#fff'
             saveInCookies: false, // default: true,
             label: '🌓', // default: ''
             autoMatchOsTheme: true // default: true
@@ -75,8 +67,6 @@
           
           const darkmode = new Darkmode(options);
           darkmode.showWidget();
-          
-        new Darkmode().showWidget();
-      }
+                }
       window.addEventListener('load', addDarkmodeWidget);
     /*黑夜模式结束*/
