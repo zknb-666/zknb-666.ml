16aa986ba67841a4cca09514c638f7ce852484dc
zknb-666 <2865467965@qq.com>
2024-03-21 00:21:45 +0800

    继续美化

--- js/main.js
+++ js/main.js
@@ -1,48 +1,32 @@
     /*加载等待*/
     document.onreadystatechange = function () {
-        $("#pageContent").hide();
-        $('.footer_div').hide();
     if (document.readyState == "complete") {    
-        $(".loader-div").hide('animate__bounceOut');
-        $("#pageContent").show();
-        $('.footer_div').show();
-        if (!$('.panel-cover').hasClass('panel-cover--collapsed'))
-        $('.panel-cover').addClass('animate__bounceIn');
+        $(".loader-div").hide('animate__flash');
     }}
     /*加载等待结束*/
+    
     /*Pjax功能*/
     $(document).pjax('a[target!=_blank]', '#pageContent', {fragment: '#pageContent',timeout: 50000,cache: false});
     $(document).on('pjax:start',function() { NProgress.start();});
     $(document).on('pjax:end',function() { NProgress.done();});
     $(document).on("pjax:complete",function(){hljs.initHighlighting();});
     /*Pjax功能结束*/
-    /*超链接触发*/
-    $("a").click(function() {
-        currentWidth = $(".panel-cover").width();
-        if (currentWidth < 2000) {
-            $(".panel-cover").addClass("panel-cover--collapsed")
-        } else {
-            $(".panel-cover").css("max-width", currentWidth);
-            $(".panel-cover").animate({
-                "max-width": "320px",
-                "width": "22%"
-            },
-            400, swing = "swing");
-        }
-        $.getScript("/js/main.js");
-    });
-    /*超链接触发结束*/
+    
     /*逻辑判断*/
-    if (location.pathname == "/")
-        {
-            $(".panel-cover").removeClass("panel-cover--collapsed");
-            $(".panel-main__content").show();
-        }
-    else
-        {
-            $(".panel-cover").addClass("panel-cover--collapsed");
-            $(".panel-main__content").hide();
-        }
+    function ishome() {
+        if (location.pathname == "/")
+            {
+                $(".panel-cover").removeClass("panel-cover--collapsed");
+                $(".panel-main__content").show();
+            }
+        else
+            {
+                $(".panel-cover").addClass("panel-cover--collapsed");
+                $(".panel-main__content").hide();
+            }
+        };
+    ishome();
+    document.addEventListener('click',ishome);
     /*逻辑判断结束*/
     
     /*打赏*/
@@ -61,6 +45,7 @@
         $(".shang_box").fadeToggle();
     }
     /*打赏结束*/
+    
     /*菜单变换*/
     document.addEventListener("scroll",function(){
     if($(document).scrollTop() == 0)
@@ -69,3 +54,4 @@
     $(".btn-menu").addClass("menu-background");
     });
     /*结束*/
+
